588d2f9f674469cfda2303cd27881017e25838a0
Régis Melgaço de Andrade <regis.melgaco@gmail.com>
2022-07-02 10:23:48 -0300

    feat: login

---
+++ web/src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+  import { Router, Route } from "svelte-routing";
+  import LoginPage from "./lib/LoginPage.svelte";
+
+  let url = "";
+</script>
+
+<main>
+  <Router {url}>
+    <Route path="/"><LoginPage /></Route>
+  </Router>
+</main>
+
+<style>
+</style>
---
+++ web/src/lib/LoginPage.svelte
@@ -0,0 +1,23 @@
+<svelte:head>
+    <script src="https://accounts.google.com/gsi/client" async defer></script>
+    <script>
+        function onLogin(resp) {
+            document.cookie = "credential="+resp.credential;
+        }
+    </script>
+</svelte:head>
+
+<div
+    id="g_id_onload"
+    data-client_id="698154937532-gj4c6ofq2t0q03ffv7vbimuf0cfrc1g8.apps.googleusercontent.com"
+    data-login_uri="http://localhost:3000"
+    data-auto_prompt="false"
+    data-callback="onLogin"
+/>
+<div
+    class="g_id_signin"
+    data-type="standard"
+    data-theme="outline"
+    data-text="sign_in_with"
+    data-logo_alignment="left"
+/>
